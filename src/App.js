16e97d7e952fe0aca07605e74b7b0bad7516617b
kumarm5 <mklkumar330@gmail.com>
2020-05-18 11:36:29 +0530

    changes for tag and app js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Tag from './components/Tag';
 
 class App extends Component {
   render() {
     return (
-      // <BrowserRouter>
-      //   <Switch>
-      //     <Route path="/" exact component={Tag} />
-      //   </Switch>
-      // </BrowserRouter>
-      <h1>React App</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Tag} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
