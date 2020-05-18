229f05ce410c0175ee3e3ee04c21649cb81b6cce
kumarm5 <mklkumar330@gmail.com>
2020-05-18 09:07:46 +0530

    Initial Commit

---
+++ src/components/Tag.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export default class Tag extends Component {
+    state = {
+        tags: []
+    }
+
+    componentDidMount() {
+        axios.get('http://kumarm5.pythonanywhere.com/api/tag/')
+            .then(res => {
+                const tags = res.data;
+                this.setState({ tags });
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.tags.map(
+                        tag => <h1>{tag.tag_name}</h1>
+                    )
+                }
+
+            </div>
+        )
+    }
+}
