77ac112974b3096dc524d56d3244ae2a496ff18f
kumarm5 <mklkumar330@gmail.com>
2020-05-18 10:59:53 +0530

    Changes for text

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Tag from './components/Tag';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Tag} />
-        </Switch>
-      </BrowserRouter>
+      // <BrowserRouter>
+      //   <Switch>
+      //     <Route path="/" exact component={Tag} />
+      //   </Switch>
+      // </BrowserRouter>
+      <h1>React App</h1>
     );
   }
 }
