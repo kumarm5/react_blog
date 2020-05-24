1e2c078251594864a46500f27b2f2314a6d0f196
kumarm5 <mklkumar330@gmail.com>
2020-05-24 07:45:03 +0530

    javascript

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Tag from './components/Tag';
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Tag} />
         </Switch>
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
