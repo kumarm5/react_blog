58af89a3d17f1399394acf8e31a52b3145cc5ead
kumarm5 <mklkumar330@gmail.com>
2020-05-25 17:05:58 +0530

    router changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route } from "react-router-dom";
 import Index from './components/Index';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -8,16 +8,12 @@ import Search from './components/Search';
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Router basename='/react_blog'>
-          <Switch>
+        <HashRouter basename='/'>
             <Route exact path="/" component={Index} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             <Route path="/search" component={Search} />
-          </Switch>
-        </Router>
-      </React.Fragment>
+        </HashRouter>
     );
   }
 }
