2e21713d3e312b67d630c5a735888d55fc7fdcc8
kumarm5 <mklkumar330@gmail.com>
2020-05-25 18:36:48 +0530

    Updates

---
+++ precache-manifest.fdeeecb7a07e4c948a68f71df1641088.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672fb312f5740ca46f57c4bd5427440d",
+    "url": "/react_blog/index.html"
+  },
+  {
+    "revision": "6cedef253c94880c9e8c",
+    "url": "/react_blog/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a981aadb1e079a4ae673",
+    "url": "/react_blog/static/css/main.506313bb.chunk.css"
+  },
+  {
+    "revision": "6cedef253c94880c9e8c",
+    "url": "/react_blog/static/js/2.9bd580dd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_blog/static/js/2.9bd580dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a981aadb1e079a4ae673",
+    "url": "/react_blog/static/js/main.5056c959.chunk.js"
+  },
+  {
+    "revision": "d46e45848f0a01659e76",
+    "url": "/react_blog/static/js/runtime-main.395f978b.js"
+  },
+  {
+    "revision": "ccda340f79271faa5c15ed84a7e4205d",
+    "url": "/react_blog/static/media/banner.ccda340f.jpg"
+  },
+  {
+    "revision": "7bff3f0bb265d5b9734cc7325095a2fd",
+    "url": "/react_blog/static/media/img_profile.7bff3f0b.jpg"
+  }
+]);
