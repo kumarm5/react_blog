a8b59f7b290348ab9ae1ceb4d45b23953ea9aee8
kumarm5 <mklkumar330@gmail.com>
2020-05-25 18:27:39 +0530

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Link } from "react-router-dom";
 import Index from './components/Index';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -8,16 +8,10 @@ import Search from './components/Search';
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Router path={process.env.PUBLIC_URL + '/react_blog'}>
-          <Switch>
-            <Route exact path="/" component={Index} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/search" component={Search} />
-          </Switch>
-        </Router>
-      </React.Fragment>
+      <HashRouter basename='/'>
+       <Route exact path="/" component={Index} />
+       <Route path="/about" component={About} />
+     </HashRouter>
     );
   }
 }
