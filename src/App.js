0771b9e16ca6c2e3993baf59038fbad5257b006f
kumarm5 <mklkumar330@gmail.com>
2020-05-25 18:10:01 +0530

    router changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Router path={process.env.PUBLIC_URL + '/'}>
+        <Router path={process.env.PUBLIC_URL + '/react_blog'}>
           <Switch>
             <Route exact path="/" component={Index} />
             <Route path="/about" component={About} />
