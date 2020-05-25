9ed6f5cf61aad52acf2d0a44a6bbffa28c82050f
kumarm5 <mklkumar330@gmail.com>
2020-05-25 21:02:57 +0530

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}><App /></Router>,
   <React.StrictMode>
     <App />
   </React.StrictMode>,
