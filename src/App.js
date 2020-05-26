f327f3ff62d28ab30abe3bf10f204d80cd7f966b
kumarm5 <mklkumar330@gmail.com>
2020-05-26 18:43:59 +0530

    HashRouter used in router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Index from './components/Index';
 import About from './components/About';
 import Contact from './components/Contact';
