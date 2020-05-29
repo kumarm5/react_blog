42dc457b2fa100345accd3e900cb860530ccedd0
kumarm5 <mklkumar330@gmail.com>
2020-05-29 23:54:18 +0530

    search page added

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,38 @@ import React, { Component } from 'react'
 import ProfileImage from '../assets/images/img_profile.jpg'
 import Header from './Header'
 import Footer from './Footer'
+import axios from 'axios';
 
 export default class Search extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            posts: [],
+            query: ''
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const data = this.state.query
+        axios.get('https://kumarm5.pythonanywhere.com/api/blog/?query=' + data)
+            .then(response => {
+                const posts = response.data;
+                this.setState({ posts });
+            })
+    }
+
+    handleInputChange = (event) => {
+        event.preventDefault()
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
+        const { query } = this.state.query
         return (
             <div>
 
@@ -23,16 +52,21 @@ export default class Search extends Component {
                     <div class="row">
                         <div class="col-md-9">
                             <h3 class="text-center">Search</h3>
-                            <form method="POST" class="form-inline">
+                            <form method="POST" class="form-inline" onSubmit={this.handleSubmit}>
                                 <div class="form-group mx-sm-3 mb-2">
                                     <label for="query" class="sr-only">Search</label>
-                                    <input type="text" class="form-control" name="query" placeholder="Enter Search" />
+                                    <input type="text" class="form-control" name="query" value={query} placeholder="Enter Search" onChange={this.handleInputChange} />
                                 </div>
                                 <button type="submit" class="btn btn-primary mb-2">Search</button>
                             </form>
 
                             <div class="mx-sm-3 mb-2">
-
+                                {
+                                    this.state.posts.map(
+                                        post =>
+                                            <p key={post.id}><a href={'#/post/'+post.id}>{post.title}</a></p>
+                                    )
+                                }
                             </div>
 
                         </div>
@@ -43,7 +77,7 @@ export default class Search extends Component {
                                     <h4>About Me</h4>
                                 </div>
                                 <div class="widget-content">
-                                    <img src={ProfileImage} class="img-fluid" />
+                                    <img src={ProfileImage} class="img-fluid" alt="pictures" />
                                     <h2><a href="/about/" class="category sidebar-btn">Mukul Kumar</a></h2>
                                     <p>My name is Mukul Kumar, I'm a software developer and technology enthusiast from India. At the moment, I'm living in Pune..</p>
                                 </div>
